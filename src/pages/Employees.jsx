376be644ff65d215f703a512e9fffb3724ce200c
Benson Osei-Mensah <kbenson643@gmail.com>
2023-08-07 10:40:33 +0000

    Preparing for view all employees page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,6 +1,16 @@
+import { EmployeesTable } from "../components/buttons/EmployeesTable";
 
 export const Employees = () => {
-  return (
-    <div>Employees</div>
-  )
-}
+	return (
+		<div className="mt-3 w-full">
+			<h1 className="text-2xl font-bold p-2 text-blue-800">
+				Employee
+			</h1>
+
+      <div className="flex flex-col p-3">
+        <EmployeesTable />
+      </div>
+
+		</div>
+	);
+};
